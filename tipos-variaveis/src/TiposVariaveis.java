f7dc797288f9aeac580f7c876c62ba8625943152
Guiroupe <guiiroupe@gmail.com>
2024-04-30 14:51:04 -0300

    Colocando os arquivos da aula de tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 1411.99;
+        
+        //Java é extremamente tipado, não esqueça!!
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //podemos colocar um cast ou mudar o tipo da variável
+
+        /* as Constantes, são valores armazenados em memória que não podem ser modificados depois de declarados. Em Java, esses valores são representados pela palavra reservada final, seguida do tipo.
+        
+        Por convenção, Constantes são sempre escritas em CAIXA ALTA.*/ 
+
+        final int VALOR_DA_TAXA = 5;
     }
 }
