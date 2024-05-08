419d931ca8b19c4767f45c5a0363348592cf5db8
Guiroupe <guiiroupe@gmail.com>
2024-05-08 15:30:16 -0300

    Adicionando o conteudo de encapsulamento e o exemplo do msn

---
+++ exemplo-msn/src/MSNMessenger.java
@@ -0,0 +1,18 @@
+public class MSNMessenger {
+	public void enviarMensagem() {
+		validarConectadoInternet();
+		System.out.println("Enviando mensagem");
+		salvarHistoricoMensagem();
+	}
+	public void receberMensagem() {
+		validarConectadoInternet();
+		System.out.println("Recebendo mensagem");
+		salvarHistoricoMensagem();
+	}
+	private void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	private void salvarHistoricoMensagem() {
+		System.out.println("Salvando o histórico da mensagem");
+	}
+}
---
+++ exemplo-msn/src/MeuComputador.java
@@ -0,0 +1,9 @@
+public class MeuComputador {
+  public static void main(String[] args) {
+		//abrindo MSN Messenger
+		MSNMessenger msn = new MSNMessenger();
+		
+		msn.enviarMensagem();
+		msn.receberMensagem();
+	}
+}
