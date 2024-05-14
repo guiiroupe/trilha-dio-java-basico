6de89aa827ee6f020cc45484aad12fb9a700f94f
Guiroupe <guiiroupe@gmail.com>
2024-05-14 17:00:02 -0300

    Adicionando o conteudo da aula de ordenacao

---
+++ set/src/ordenacao/CadastroProduto.java
@@ -0,0 +1,60 @@
+package ordenacao;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProduto {
+  private Set<Produto> produtoSet;
+
+  public CadastroProduto() {
+    this.produtoSet = new HashSet<>();
+  }
+
+  public void adicionarProduto(long cod, String nome, double preco, int quantidade){
+    produtoSet.add(new Produto(cod, nome, preco, quantidade));
+  }
+
+  public Set<Produto> exibirProdutosPorNome(){
+    //O treeSet sabe organizar, não precisamos do collection. fizemos via comparable
+    Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+    return produtosPorNome;
+  }
+
+  public Set<Produto> exibirProdutosPorPreco(){
+    Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+    produtosPorPreco.addAll(produtoSet);
+    return produtosPorPreco;
+  }
+  
+  public static void main(String[] args) {
+    CadastroProduto cadastroProdutos = new CadastroProduto();
+
+    // Adicionando produtos ao cadastro
+    cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+    cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+    cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+    cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+    // Exibindo todos os produtos no cadastro
+    System.out.println(cadastroProdutos.produtoSet);
+
+    // Exibindo produtos ordenados por nome
+    System.out.println(cadastroProdutos.exibirProdutosPorNome());
+
+    // Exibindo produtos ordenados por preço
+    System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+  }
+
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+  @Override
+  public int compare(Produto p1, Produto p2) {
+    return Double.compare(p1.getPreco(), p2.getPreco());
+  }
+
+  
+}
