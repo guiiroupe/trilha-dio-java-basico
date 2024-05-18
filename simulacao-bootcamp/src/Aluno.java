ec5496026b60def0e33f5911fff6a23b46a59df3
Guiroupe <guiiroupe@gmail.com>
2024-05-18 14:45:59 -0300

    subindo o projeto de simulacao de um bootcamp

---
+++ simulacao-bootcamp/src/Aluno.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+public class Aluno {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new HashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new HashSet<>();
+
+    public void inscreverNoBootcamp(Bootcamp bootcamp) {
+        conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
+
+    public void avancar() {
+        if (!conteudosInscritos.isEmpty()) {
+            Conteudo conteudo = conteudosInscritos.iterator().next();
+            conteudosConcluidos.add(conteudo);
+            conteudosInscritos.remove(conteudo);
+        } else {
+            System.err.println("Nenhum conteúdo para avançar!");
+        }
+    }
+
+    public double calcularTotalXp() {
+        double totalXp = 0;
+        for (Conteudo conteudo : conteudosConcluidos) {
+            totalXp += conteudo.calcularXp();
+        }
+        return totalXp;
+
+        // Alternativa com stream:
+        // return conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXp).sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(nome, aluno.nome) &&
+               Objects.equals(conteudosInscritos, aluno.conteudosInscritos) &&
+               Objects.equals(conteudosConcluidos, aluno.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+}
+
+
